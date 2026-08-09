8359ccae8abd3fdb352054f26739a39a5ef04c6d
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-08-09 12:50:05 +0900

    Expose QC and example APIs

--- src/shimaflora/__init__.py
+++ src/shimaflora/__init__.py
@@ -2,6 +2,8 @@
 
 from .morphology import measure_shape, width_profile, width_at
 from .pattern import ColorPatternModel, measure_pattern
+from .qc import overlay_roi, overlay_morphology, overlay_pattern
+from .examples import synthetic_flower
 
 __all__ = [
     "measure_shape",
@@ -9,6 +11,10 @@ __all__ = [
     "width_at",
     "ColorPatternModel",
     "measure_pattern",
+    "overlay_roi",
+    "overlay_morphology",
+    "overlay_pattern",
+    "synthetic_flower",
 ]
 
 __version__ = "0.1.0"
